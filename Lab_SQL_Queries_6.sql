e1f56acd9bdcfc532375cca29def805c4f218cbb
Andrew Ashdown <47285596+surelybassy@users.noreply.github.com>
2020-11-30 19:37:23 +0100

    SQL Lab 6 added

---
+++ Lab_SQL_Queries_6.sql
@@ -0,0 +1,27 @@
+drop table if exists films_2020;
+CREATE TABLE `films_2020` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` int(6),
+  `rental_rate` decimal(4,2),
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT NULL,
+  PRIMARY KEY (`film_id`),
+  CONSTRAINT FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8;
+
+ALTER TABLE films_2020 ADD rental_duration INT NOT NULL DEFAULT 3;
+ALTER TABLE films_2020 ADD offer_price FLOAT NOT NULL DEFAULT 2.99;
+ALTER TABLE films_2020 ADD replacement_cost FLOAT NOT NULL DEFAULT 8.99;
+
+# Check to see if everything is added.
+
+SELECT * FROM films_2020;
+
+
+# Boom.
